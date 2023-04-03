f719cd25ca8bf406d562d1450b73ac2e7110d35c
Lena <kotukovaea@gmail.com>
2023-04-03 21:14:16 +0400

    Add Program

---
+++ Task1/Task1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону прямоугольника: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону прямоугольника: ");
+            int b = int.Parse(Console.ReadLine());
+            if (a <= 0 || b <= 0)
+            {
+                Console.WriteLine("Вы ввели 0 или отрицательное число");
+            }
+            else
+            {
+                int result = a * b;
+                Console.WriteLine("Площадь прямоугольника: " + result);
+            }
+
+        }
+    }
+}

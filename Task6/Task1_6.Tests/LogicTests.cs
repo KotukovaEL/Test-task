6d8e29a0176bb863f15b904cf347d95ea132173b
Lena <kotukovaea@gmail.com>
2023-08-16 16:54:00 +0400

    Fix Task6

--- Task6/Task1_6.Tests/LogicTests.cs
+++ Task6/Task1_6.Tests/LogicTests.cs
@@ -2,17 +2,30 @@ using System;
 using Xunit;
 
 
+
 namespace Task1_6.Tests
 {
     public class LogicTests
     {
         [Theory]
         [InlineData("Bold", DisplayType.Bold)]
-        public void Should_read_correct_DisplayType(string value, string displayType)
+        [InlineData("Italic", DisplayType.Italic)]
+        [InlineData("Underline", DisplayType.Underline)]
+        public void Should_read_correct_DisplayType(string value, DisplayType expectedType)
         {
-            var boolResult = Logic.TryReadType(value, out var type);
+            var boolResult = Logic.TryReadType(value, out DisplayType actualType);
             Assert.True(boolResult);
-            Assert.Equal(displayType, type);
-        }       
+            Assert.Equal(expectedType, actualType);
+        }
+
+        [Theory]
+        [InlineData("fdg")]
+        [InlineData("Italdfadsic")]
+        [InlineData("sfd")]
+        public void Should_not_read_incorrect_DisplayType(string value)
+        {
+            var boolResult = Logic.TryReadType(value, out DisplayType actualType);
+            Assert.False(boolResult);
+        }
     }
 }
